a12354df6be045c24a966b525383c5556571e5d2
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-12 13:31:10 +0000

    Update Power.h

--- src/Power.h
+++ src/Power.h
@@ -1,1 +1,28 @@
-
+#ifndef PowerLib_h
+#define PowerLib_h
+#include<Arduino.h>
+class Power {
+public:
+    Power(double *I, double *V, int length);;
+    double iEff();
+    double vEff();
+    double iMoy();
+    double vMoy();
+    double P();
+    double Q();
+    double S();
+    double pf();
+    void print();
+    void push(double i,double v);
+    void clear();
+    double Req();
+    double Xeq();
+    double Zeq();
+public:
+    double* _I;
+    double* _V;
+    int _length;
+    double calculEff(double values1[], double values2[]);
+    double calculMean(double values[]);
+};
+#endif
